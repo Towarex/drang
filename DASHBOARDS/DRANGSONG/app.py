0b6c9880a6a00c1e48048a6c0ebafd13c72eb80a
Towarex <aleksandra.towarek@gazeta.pl>
2026-03-09 15:57:05 +0100

    port edit

--- DASHBOARDS/DRANGSONG/app.py
+++ DASHBOARDS/DRANGSONG/app.py
@@ -117,6 +117,6 @@ def draw_pair(pierwiastek1, pierwiastek2, dane):
 
   return fig
 
-
-app.run(jupyter_mode="external", debug=True)
+port = int(os.environ.get("PORT", 8050))
+app.run(jupyter_mode="external", host="0.0.0.0", port=port, debug=True)
 
