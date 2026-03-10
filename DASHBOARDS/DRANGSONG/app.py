f2ed3b315d0b79aab3e8089d1faa19b60d381142
Towarex <166216365+Towarex@users.noreply.github.com>
2026-03-10 11:59:50 +0100

    Update app.py
    Signed-off-by: Towarex <166216365+Towarex@users.noreply.github.com>

--- DASHBOARDS/DRANGSONG/app.py
+++ DASHBOARDS/DRANGSONG/app.py
@@ -118,5 +118,7 @@ def draw_pair(pierwiastek1, pierwiastek2, dane):
   return fig
 
 port = int(os.environ.get("PORT", 8050))
-app.run(jupyter_mode="external", host="0.0.0.0", port=port, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
 
